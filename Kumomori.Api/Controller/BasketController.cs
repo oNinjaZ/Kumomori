9398006bd9e564ba6ed0da1386664184148afa2b
oNinjaZ <marcbelzamour@yahoo.com>
2022-07-06 08:00:52 -0400

    update GetBasket

--- Kumomori.Api/Controller/BasketController.cs
+++ Kumomori.Api/Controller/BasketController.cs
@@ -16,14 +16,42 @@ public class BasketController : BaseApiController
     [HttpGet]
     public async Task<ActionResult<Basket>> GetBasket()
     {
-        var basket = await _context.Baskets
-            .Include(i => i.Items)
-            .ThenInclude(b => b.Book)
-            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+        var basket = await RetrieveBasket();
 
         if (basket is null)
             return NotFound();
-        
+
         return basket;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddItemToBasket(int bookId, int quantity)
+    {
+        var basket = await RetrieveBasket();
+        if (basket is null)
+            basket = CreateNewBasket();
+
+        // get book
+        // add item
+        // save basket
+
+        return await Task.FromResult(StatusCode(201));
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> RemoveBasketItem(int bookId, int quantity)
+    {
+        // get basket
+        // remove item or reduce quantity
+        // save basket
+        return Ok();
+    }
+
+    private async Task<Basket?> RetrieveBasket()
+    {
+        return await _context.Baskets
+                    .Include(i => i.Items)
+                    .ThenInclude(b => b.Book)
+                    .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
     }
 }
