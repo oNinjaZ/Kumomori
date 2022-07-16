2d5b65d4275148631bbe1b59dab0c59059cc5fe9
oNinjaZ <marcbelzamour@yahoo.com>
2022-07-15 20:54:21 -0400

    cmt

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,14 +1,25 @@
-import { Delete } from "@mui/icons-material";
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Add, Delete, Remove } from "@mui/icons-material";
+import { Box, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { useState } from "react";
+import agent from "../../app/api/agent";
 import { useStoreContext } from "../../app/context/StoreContext";
 
 export default function BasketPage() {
-    const {basket} = useStoreContext();
+    const {basket, setBasket, removeItem} = useStoreContext();
+    const[loading, setLoading] = useState(false);
+    
+    function handleAddItem(bookId: number) {
+        setLoading(true);
+        agent.Basket.addItem(bookId)
+            .then(basket => setBasket(basket))
+            .catch(error => console.log(error))
+            
+    }
 
     if (!basket) {
         return <Typography>Your cart is empty</Typography>;
     }
-
+    
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }}>
@@ -16,7 +27,7 @@ export default function BasketPage() {
                     <TableRow>
                         <TableCell>Book</TableCell>
                         <TableCell align="right">Price</TableCell>
-                        <TableCell align="right">Quantity</TableCell>
+                        <TableCell align="center">Quantity</TableCell>
                         <TableCell align="right">Subtotal</TableCell>
                         <TableCell align="right"></TableCell>
                     </TableRow>
@@ -28,10 +39,21 @@ export default function BasketPage() {
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                         >
                             <TableCell component="th" scope="row">
-                                {item.title}
+                                <Box display='flex' alignItems='center'>
+                                    <img src={item.coverUrl} alt={item.title} style={{height: 50, marginRight: 20}}/>
+                                    <span>{item.title}</span>
+                                </Box>
                             </TableCell>
                             <TableCell align="right">円{item.price}</TableCell>
-                            <TableCell align="right">{item.quantity}</TableCell>
+                            <TableCell align="center"> 
+                                <IconButton color='error'>
+                                    <Remove />
+                                </IconButton>
+                                {item.quantity}
+                                <IconButton color='error'>
+                                    <Add />
+                                </IconButton>
+                            </TableCell>
                             <TableCell align="right">円{item.price * item.quantity}</TableCell>
                             <TableCell align="right">
                                 <IconButton color='error'>
